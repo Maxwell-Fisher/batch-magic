ecce98d2077b049b15c860b55074e1f0b605b7e6
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2021-10-20 11:36:23 -0600

    Created `memoryCorruption`

---
+++ memoryCorruption.bat
@@ -0,0 +1,12 @@
+title Memory corruption - @Maxwellcrafter
+mode con: cols=99 lines=10
+for /F %%a in ('echo prompt $E ^| cmd') do set "esc=%%a"
+setLocal enableDelayedExpansion
+set array=AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSs
+set array=AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789 !@#$%^&*()-=_+`~[]{}\|;':",./<>?-=,./
+echo off
+cls
+ping 127.1 -n 4 >nul
+:a
+echo !esc![!random:~1,1!;!random:~1,1!!random:~1,1!H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m!array:~%random:~0,1%%random:~1,1%,1!!esc![0m
+goto a

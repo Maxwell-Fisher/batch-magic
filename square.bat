c974281f2a969b0c23f31f175200449614743be2
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2021-10-20 12:55:10 -0600

    Created `square`

---
+++ square.bat
@@ -0,0 +1,84 @@
+title Memory corruption - @Maxwellcrafter
+mode con: cols=20 lines=11
+for /F %%a in ('echo prompt $E ^| cmd') do set "esc=%%a"
+setLocal enableDelayedExpansion
+set hold=ping 127.1 -n 1
+echo off
+cls
+ping 127.1 -n 4 >nul
+
+:a
+echo !esc![1;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;3H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;5H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;7H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;9H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;11H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;13H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;15H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;17H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![1;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![2;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![3;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![4;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![5;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![6;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![7;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![8;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![9;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;19H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;17H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;15H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;13H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;11H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;9H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;7H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;5H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;3H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![10;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![9;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![8;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![7;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![6;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![5;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![4;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![3;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+echo !esc![2;1H!esc![48;5;!random:~1,2!m!esc![35;5;!random:~1,2!m  !esc![0m
+!hold! >nul
+goto a
+::This is horribly inefficient, but it works

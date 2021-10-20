7d62ed04d4e5fa2674f7c5c2c3a68e3892d181dc
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2021-10-20 09:13:24 -0600

    Fixed so that this runs in 256 colour mode

--- lines.bat
+++ lines.bat
@@ -4,14 +4,8 @@ setLocal enableDelayedExpansion
 echo off
 cls
 ping 127.1 -n 4 >nul
-set red=!esc![31m!esc![41m !esc![0m
-set green=!esc![32m!esc![42m !esc![0m
-set blue=!esc![34m!esc![44m !esc![0m
-set cyan=!esc![36m!esc![46m !esc![0m
-set magenta=!esc![35m!esc![45m !esc![0m
-set yellow=!esc![33m!esc![43m !esc![0m
 cls
 
 :a
-echo !esc![!random:~0,2!;!random:~0,2!H!esc![35;5;!random:~0,2!m
+echo !esc![!random:~0,2!;!random:~0,2!H!esc![48;5;!random:~0,2!m
 goto a
